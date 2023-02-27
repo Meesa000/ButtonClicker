014df2290c72dd7257b411083f6bea75f2cf07a9
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2023-02-27 17:51:32 +0000

    Button clicker and popup window added

--- src/main/java/com/example/buttonclicker/HelloApplication.java
+++ src/main/java/com/example/buttonclicker/HelloApplication.java
@@ -5,33 +5,48 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Button Clicker");
-
-        //button
-        Button button = new Button("CLICK ME");
-
-
-       //set the scene stackpane root
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-
-        Scene scene = new Scene(root, 300,350);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-    }
+    Scene scene1;
+    Scene scene2;
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Button button = new Button("CLICK ME");
+        button.setOnAction(event -> AlertBox.display("Alert box", "PLEASE CLOSE THIS WINDOW."));
+
+
+        //layout
+        StackPane root = new StackPane();
+        root.getChildren().addAll(button);
+        scene1 = new Scene(root, 300,300);
+
+
+
+
+        primaryStage.setTitle("Button Clicker");
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+
+
+
+
+    }
+
+
 }
